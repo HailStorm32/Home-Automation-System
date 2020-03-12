d2faa43fa318706e2938cd5c1026d65c555b1e1d
Demetri Van Sickle <demetriv.s.7@gmail.com>
2020-03-12 16:40:41 -0700

    Began to add sql code

--- CommandCenter/include/functions.h
+++ CommandCenter/include/functions.h
@@ -1,4 +1,11 @@
-bool hubSetup(RF24 *radioP, uint8_t numOfHubSetups = 1)
+struct hub
+{
+    uint8_t address;
+    string name;
+};
+
+bool hubSetup(RF24 *radioP, uint8_t numOfHubSetups);
+bool retrieveAddrs(MYSQL *sqlConnP, hub *hasHubsP);
 
 
 //======================================================================
@@ -13,7 +20,7 @@ bool hubSetup(RF24 *radioP, uint8_t numOfHubSetups = 1)
 //  true -- if setup succeeded
 //  false -- if setup failed 
 //=======================================================================
-bool hubSetup(RF24 *radioP, uint8_t numOfHubSetups = 1)
+bool hubSetup(RF24 *radioP, uint8_t numOfHubSetups)
 {
     uint8_t newHubAddr = 0;
     uint8_t savedAddrs [255];
@@ -23,6 +30,11 @@ bool hubSetup(RF24 *radioP, uint8_t numOfHubSetups = 1)
         savedAddrs[indx] = 0;
     }
 
+    if(numOfHubSetups <= 0)
+    {
+        std::cout << "Number of hubs to set up needs to be greater than 0" << std::endl;
+    }
+
     system("clear");
     std::cout << "Begining setup of hub.." << std::endl;
 
@@ -30,7 +42,7 @@ bool hubSetup(RF24 *radioP, uint8_t numOfHubSetups = 1)
     std::this_thread::sleep_for(std::chrono::milliseconds(200));
 
     radioP->setRetries(1, 15);
-    radio->setChannel(124);
+    radioP->setChannel(124);
 
     std::cout << "Creating address..." << std::endl;
 
@@ -41,4 +53,59 @@ bool hubSetup(RF24 *radioP, uint8_t numOfHubSetups = 1)
     //TODO: Send new address to hub as well as the rest of the addresses
     
     //TODO: Start placment mode (use a function)
+
+    return true;
+}
+
+
+//======================================================================
+//Description: Retrieves hub address from database
+//
+//Arguments:
+//  (IN) sqlConnP -- pointer to mysql connetion
+//  (OUT) hasHubsP -- pointer to struct that holds hub data
+//
+//Return:
+//  true -- succeeded
+//  false -- failed
+//=====================================================================
+bool retrieveAddrs(MYSQL *sqlConnP, hub *hasHubsP)
+{
+    MYSQL_RES *sqlResult;
+    MYSQL_ROW sqlRow;
+    int genIndx = 0;
+
+    if(mysql_ping(sqlConnP))
+    {
+       std::cout << "\nERROR: Ping to database from 'retrieveAddrs' failed!"
+           << std::endl;
+       mysql_close(sqlConnP);
+       return false;
+    }
+
+    //Get addresses from database
+    if(mysql_query(sqlConnP, "SELECT * FROM hubs") && QUERY_ERROR_CHECK)
+    {
+        std::cout << "\nERROR: sql query in 'retrieveAddrs' failed!" << std::endl;
+        mysql_close(sqlConnP);
+        return false;
+    }
+    sqlResult = mysql_store_result(sqlConnP);
+    sqlRow = mysql_fetch_row(sqlResult);
+
+    while((sqlRow = mysql_fetch_row(sqlResult)) != NULL)
+    {
+        if(genIndx >= 255)
+        {
+            std::cout << "\nWARNING: Number of addresses in database exceeds 255!" 
+                " Some addresses may be missing!" << std::endl;
+            mysql_free_result(sqlResult);
+            break;
+        }
+
+        hasHubsP[genIndx].address = std::stoi(sqlRow[0]);
+        hasHubsP[genIndx].name = sqlRow[1];
+        genIndx++;
+    }
+    return true;
 }
--- CommandCenter/src/main.cpp
+++ CommandCenter/src/main.cpp
@@ -10,16 +10,34 @@
 #include <wiringPi.h>
 #include <RF24.h>
 #include <AES.h>
+#include <mysql/mysql.h>
+#include <config.h>
 
-#include "functions.h"
 
 const uint8_t STATUS_LED = 1;
 const uint8_t ERROR_LED = 4;
 const uint8_t NUM_OF_MENU_ITEMS = 2;
 const uint8_t VALID_MENU_ITEMS[] = {1,2};
+const bool QUERY_ERROR_CHECK = true;
+
+#include "functions.h"
 
 int main()
 {
+    bool exitFlag = false;
+    int menuItemNum;
+    int genIndx = 0;
+    string userEntry = "";
+    string sqlQuery;
+    bool haveAddrs;
+    uint8_t hubAddrs[255] = {};
+    uint8_t myAddress = 0;
+    hub hasHubs[255];
+
+    MYSQL *sqlConn;
+    MYSQL_RES *sqlResult;
+    MYSQL_ROW sqlRow;
+
     //Setup
     wiringPiSetup();
 
@@ -33,13 +51,90 @@ int main()
     //Code
     digitalWrite(STATUS_LED, LOW);
     digitalWrite(ERROR_LED, LOW);
-   
-    //TODO: Check database for server address. If already generated, take it.
-    //If not generated, generate it and then save it to database
 
-    bool exitFlag = false;
-    int menuItemNum;
-    string userEntry = "";
+    //Init the hub address array
+    for(int indx = 0; indx < 255; indx++)
+    {
+        hubAddrs[indx] = 0;
+    }
+    
+    if(!QUERY_ERROR_CHECK)
+    {
+        std::cout << "\nWARNING: Error checking for database queries is off!"
+            << std::endl;
+    }
+
+
+    //Make connection to database
+    sqlConn = mysql_init(sqlConn);
+    mysql_options(sqlConn, MYSQL_READ_DEFAULT_GROUP, "CommandCenter");
+    if(!mysql_real_connect(sqlConn, DB_IP.c_str(), DB_USER.c_str(),
+            DB_PASS.c_str(), DB_NAME.c_str(), 8080, NULL, 0))
+    {
+        std::cout << "\nERROR: Cant connect to SQL database!" << std::endl;
+        mysql_close(sqlConn);
+        return 1;
+    }
+
+    //Check to see if we have setup any hubs
+    sqlQuery = "SELECT * FROM variables WHERE name='numOfSetupHubs'";
+    if(mysql_query(sqlConn, sqlQuery.c_str()) 
+            && QUERY_ERROR_CHECK)
+    {
+        std::cout << "\nERROR: sql query failed to get 'numOfSetupHubs'!" 
+            << std::endl;
+        mysql_close(sqlConn);
+        return 1;
+    }
+    sqlResult = mysql_store_result(sqlConn);
+    sqlRow = mysql_fetch_row(sqlResult);
+    
+    //Get the addresses if we have them
+    if(std::stoi(sqlRow[1]) > 0)
+    {
+        haveAddrs = true;
+        mysql_free_result(sqlResult);
+        
+        retrieveAddrs(sqlConn, hasHubs);
+        
+        myAddress = hasHubs[0].address;
+    }
+    else
+    {
+        haveAddrs = false;
+        mysql_free_result(sqlResult);
+        
+        //TODO: Generate random number using library for server address
+        
+        myAddress = 42; //Place holder value
+        sqlQuery = "INSERT INTO hubs VALUES('" + std::to_string(myAddress) +
+            "','server')";
+
+        if(mysql_query(sqlConn, sqlQuery.c_str()) && QUERY_ERROR_CHECK)
+        {
+            std::cout << "\nERROR: sql query failed to update hub addresses!"
+                << std::endl;
+            std::cout << "DEBUG: Continuing anyway... Data may be missing" 
+                << endl;
+            mysql_close(sqlConn);
+            return 1;
+        }
+        
+        sqlQuery = "UPDATE variables SET value='1' WHERE name='numOfSetupHubs'";
+        
+        if(mysql_query(sqlConn, sqlQuery.c_str()) && QUERY_ERROR_CHECK)
+        {
+            std::cout << "\nERROR: sql query failed to update variable!"
+                << std::endl;
+            std::cout << mysql_info(sqlConn) << std::endl;
+            std::cout << mysql_error(sqlConn) << std::endl;
+            std::cout << mysql_errno(sqlConn) << std::endl;
+            mysql_close(sqlConn);
+            return 1;
+        }
+    }
+
+
 
     //Clear screen
     system("clear");
@@ -48,7 +143,8 @@ int main()
     {
         userEntry = "";
 
-        std::cout << "Welcome to the Home Automation System Server Interface!" << std::endl;
+        std::cout << "Welcome to the Home Automation System Server Interface!" 
+            << std::endl;
         std::cout << "Menu:" << std::endl;
         std::cout << "1). Setup new hub \n"
             "2). Exit" << std::endl;
